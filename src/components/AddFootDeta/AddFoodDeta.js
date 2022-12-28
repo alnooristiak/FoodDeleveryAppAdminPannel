181a5b07c791abba4c9adb841d3133a4c97f5947
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-12-28 22:24:28 +0600

    push data firebase without error

--- src/components/AddFootDeta/AddFoodDeta.js
+++ src/components/AddFootDeta/AddFoodDeta.js
@@ -11,6 +11,7 @@ const AddFoodDeta = () => {
     const [foodPrice, setFoodPrice] = useState('');
     const [foodCategory, setFoodCategory] = useState('');
     const [foodImage, setFoodImage] = useState(null);
+    const [foodImageUrl, setFoodImageUrl] = useState('')
 
     // Resturent Detail Hooks
     const [rstName, setRstName] = useState('');
@@ -21,40 +22,59 @@ const AddFoodDeta = () => {
     // console.log(foodName);
     const handleSubmit = (e) => {
         // e.preventdefault() is not working
-        if(foodImage == null){
+        if (foodImage == null) {
             alert("please Select an Image");
             return
-        }else{
+        } else {
             const imageRef = ref(storage, `foodImage/${foodImage.name}`)
-            uploadBytes(imageRef,foodImage)
-            .then(()=>{
-                alert("Image added suxssesfully")
-                getDownloadURL(imageRef)
-                    .then((url)=> {
-                        console.log(url)
-                    })
-            })
-            .catch((error) => {
-                alert(error.message)
-            })
+            uploadBytes(imageRef, foodImage)
+                .then(() => {
+                    alert("Image added suxssesfully")
+                    getDownloadURL(imageRef)
+                        .then((url) => {
+                            // console.log(url)
+                            // setFoodImageUrl(url)
+
+                            // get all datas 
+                            const foodDatas = {
+                                foodName,
+                                foodDescrieption,
+                                foodPrice,
+                                foodCategory,
+                                foodImageUrl: url,
+                                rstName,
+                                rstDetail,
+                                rstPhone,
+                            }
+                            try {
+                                const docRef = addDoc(collection(db, 'FoodData'), foodDatas);
+                                alert("document weitten with ID:", docRef.id);
+                            } catch (error) {
+                                alert("you find an err", error);
+                            }
+                        })
+                })
+                .catch((error) => {
+                    alert(error.message)
+                })
         }
-        const foodDatas = {
-            // foodName,
-            // foodDescrieption,
-            // foodPrice,
-            // foodCategory,
-            // foodImage,
-            // rstName,
-            // rstDetail,
-            // rstPhone,
-        }
+        // const foodDatas = {
+        //     // foodName,
+        //     // foodDescrieption,
+        //     // foodPrice,
+        //     // foodCategory,
+        //     // foodImage,
+        //     // rstName,
+        //     // rstDetail,
+        //     // rstPhone,
+        // }
         // console.log(foodDatas)
-        try{
-            const docRef = addDoc(collection(db, 'FoodData'), foodDatas);
-            alert("document weitten with ID:", docRef.id);
-        }catch (error){
-            alert("you find an err", error);
-        }
+        // try{
+        //     const docRef = addDoc(collection(db, 'FoodData'), foodDatas);
+        //     alert("document weitten with ID:", docRef.id);
+        // }catch (error){
+        //     alert("you find an err", error);
+        // }
     }
 
     return (
@@ -64,96 +84,96 @@ const AddFoodDeta = () => {
                 {/* Food Details */}
                 <h2 className='mt-4'>Food Details</h2>
                 <div>
-                    <label 
-                    className='d-block py-2 text-primary fw-bolder'>
+                    <label
+                        className='d-block py-2 text-primary fw-bolder'>
                         Food Name
                     </label>
                     <input
-                    onChange={(e)=> setFoodName(e.target.value)}
-                    type='text'
-                    placeholder='Food Name' />
+                        onChange={(e) => setFoodName(e.target.value)}
+                        type='text'
+                        placeholder='Food Name' />
                     <br />
 
-                    <label 
-                    className='d-block py-2 text-primary fw-bolder'>
+                    <label
+                        className='d-block py-2 text-primary fw-bolder'>
                         Food Description
                     </label>
-                    <input 
-                    onChange={(e)=> setFoodDescrieption(e.target.value)}
-                    type='text' 
-                    placeholder='Food Description' />
+                    <input
+                        onChange={(e) => setFoodDescrieption(e.target.value)}
+                        type='text'
+                        placeholder='Food Description' />
                     <br />
 
-                    <label 
-                    className='d-block py-2 text-primary fw-bolder'>
+                    <label
+                        className='d-block py-2 text-primary fw-bolder'>
                         Food Price
                     </label>
-                    <input 
-                    onChange={(e)=> setFoodPrice(e.target.value)}
-                    type='number'  
-                    placeholder='Food Price' />
+                    <input
+                        onChange={(e) => setFoodPrice(e.target.value)}
+                        type='number'
+                        placeholder='Food Price' />
                     <br />
 
-                    <label 
-                    className='d-block py-2 text-primary fw-bolder'>
+                    <label
+                        className='d-block py-2 text-primary fw-bolder'>
                         Food Category
                     </label>
-                    <input 
-                    onChange={(e)=> setFoodCategory(e.target.value)}
-                    type='text' 
-                    placeholder='Food Category' />
+                    <input
+                        onChange={(e) => setFoodCategory(e.target.value)}
+                        type='text'
+                        placeholder='Food Category' />
                     <br />
-                    
-                    <label 
-                    className='d-block py-2 text-primary fw-bolder'>
+
+                    <label
+                        className='d-block py-2 text-primary fw-bolder'>
                         Food Image
                     </label>
-                    <input 
-                    onChange={(e)=> setFoodImage(e.target.files[0])}
-                    type='file' 
-                    placeholder='Food Image' />
+                    <input
+                        onChange={(e) => setFoodImage(e.target.files[0])}
+                        type='file'
+                        placeholder='Food Image' />
                     <br />
                 </div>
 
                 {/* Resturents Details */}
                 <h4 className='mt-3'>Resturents Details</h4>
                 <div>
-                    <label 
-                    className='d-block py-2 text-danger fw-bolder '>
+                    <label
+                        className='d-block py-2 text-danger fw-bolder '>
                         Resturent Name
                     </label>
-                    <input 
-                    onChange={(e)=> setRstName(e.target.value)}
-                    type='text' 
-                    placeholder='Resturent Name' />
+                    <input
+                        onChange={(e) => setRstName(e.target.value)}
+                        type='text'
+                        placeholder='Resturent Name' />
                     <br />
 
-                    <label 
-                    className='d-block py-2 text-danger fw-bolder '>
+                    <label
+                        className='d-block py-2 text-danger fw-bolder '>
                         Resturent Details
                     </label>
-                    <input 
-                    onChange={(e)=> setRstDetail(e.target.value)}
-                    type='text' 
-                    placeholder='Resturent Details' />
+                    <input
+                        onChange={(e) => setRstDetail(e.target.value)}
+                        type='text'
+                        placeholder='Resturent Details' />
                     <br />
 
-                    <label 
-                    className='d-block py-2 text-danger fw-bolder '>
+                    <label
+                        className='d-block py-2 text-danger fw-bolder '>
                         Resturent Phone Number
                     </label>
-                    <input 
-                    onChange={(e)=> setRstPhone(e.target.value)}
-                    type='text' 
-                    placeholder='Resturent Phone Number' />
+                    <input
+                        onChange={(e) => setRstPhone(e.target.value)}
+                        type='text'
+                        placeholder='Resturent Phone Number' />
                     <br />
                 </div>
 
                 <div className='my-3'>
-                    <button 
-                    onClick={handleSubmit}
-                    type="button" 
-                    className='fw-bolder p-3 btn btn-success'>
+                    <button
+                        onClick={handleSubmit}
+                        type="button"
+                        className='fw-bolder p-3 btn btn-success'>
                         Add Food Data
                     </button>
                 </div>
